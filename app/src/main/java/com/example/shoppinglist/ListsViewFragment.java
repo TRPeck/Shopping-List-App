c61585194c0d03f245e722f13e296feaf000c8a4
TRPeck <taylorrpeck@gmail.com>
2020-10-25 19:21:47 -0700

    Corrected messed up naming of files and elements. Also corrected misplaced widgets.

--- app/src/main/java/com/example/shoppinglist/ListsViewFragment.java
+++ app/src/main/java/com/example/shoppinglist/ListsViewFragment.java
@@ -7,9 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-public class ListViewFragment extends Fragment {
+public class ListsViewFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,7 +16,7 @@ public class ListViewFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_view, container, false);
+        return inflater.inflate(R.layout.fragment_lists_view, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
